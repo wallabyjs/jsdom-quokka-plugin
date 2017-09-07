bc63df27f11867a3398d69ee79dbc0f64f62b014
Romain Fiévé <rfi@eb00112.emakina.com>
2017-09-07 11:20:49 +0200

    Added html from file feature

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 module.exports = {
   before: config => {
+    const fs = require('fs');
     const pluginConfig = config.jsdom || {};
     const jsdomConfig = pluginConfig.config || {};
-    const html = pluginConfig.html || '<!doctype html><html><head><meta charset="utf-8"></head><body><div id="root"></div></body></html>';
+    const fileData = jsdomConfig.file && fs.readFileSync(jsdomConfig.file).toString();
+    const html = fileData || pluginConfig.html || '<!doctype html><html><head><meta charset="utf-8"></head><body><div id="root"></div></body></html>';
     const jsdom = require('jsdom/lib/old-api.js');
     const document = jsdom.jsdom(html, jsdomConfig);
 
     Object.keys(document.defaultView).forEach((property) => {
-      if (typeof global[property] === 'undefined') {
-        global[property] = document.defaultView[property];
-      }
+        if (typeof global[property] === 'undefined') {
+            global[property] = document.defaultView[property];
+        }
     });
 
     global.navigator = {
-      userAgent: pluginConfig.userAgent || 'quokka.js'
+        userAgent: pluginConfig.userAgent || 'quokka.js'
     };
 
     if (!console.debug) {
-      console.debug = console.log;
+        console.debug = console.log;
     }
   }
 };
