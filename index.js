35891ca04c8707caccb686a479cd402634a5d5f3
Artem Govorov <artem.govorov@gmail.com>
2018-12-28 09:19:46 +1000

    Migrated to jsdom v13.1

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ module.exports = {
     const fs = require('fs');
     const pluginConfig = config.jsdom || {};
     const jsdomConfig = pluginConfig.config || {};
+    jsdomConfig.url = jsdomConfig.url || 'http://localhost/';
     const fileData = (pluginConfig.file || jsdomConfig.file) && fs.readFileSync(pluginConfig.file || jsdomConfig.file).toString();
     const html = fileData || pluginConfig.html || '<!doctype html><html><head><meta charset="utf-8"></head><body><div id="root"></div></body></html>';
-    const jsdom = require('jsdom/lib/old-api.js');
-    const document = jsdom.jsdom(html, jsdomConfig);
+    const jsdom = require('jsdom');
+    const document = new jsdom.JSDOM(html, jsdomConfig).window.document;
 
     properties.forEach((property) => {
       if (property !== 'root' && typeof global[property] === 'undefined') {
